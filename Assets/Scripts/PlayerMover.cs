371a91e2316d72cc463bb1847f0b03f89a348df5
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-10-10 20:52:48 +0300

    Create third person camera

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -6,10 +6,12 @@ public class PlayerMover : MonoBehaviour
     #region Fields
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpPower;
+    [SerializeField] private Transform mainCamera;
 
     private Rigidbody _rigidbody;
     private Vector3 _movingForce;
     private Vector3 _jumpForce;
+    private Vector3 _eulerRotation;
     private bool _needToJump;
     #endregion
 
@@ -29,7 +31,11 @@ public class PlayerMover : MonoBehaviour
     private void FixedUpdate()
     {
         if(_movingForce != Vector3.zero)
+        {
+            _eulerRotation.y = mainCamera.rotation.eulerAngles.y;
+            Rotate(_eulerRotation);
             Move(_movingForce);
+        }
     }
 
     private void UpdateInputParameters()
@@ -42,6 +48,7 @@ public class PlayerMover : MonoBehaviour
         _needToJump = Input.GetKeyDown(KeyCode.Space);
     }
     private void Move(Vector3 movingForce) => transform.Translate(moveSpeed * Time.fixedDeltaTime * movingForce);
+    private void Rotate(Vector3 eulerRotation) => transform.rotation = Quaternion.Euler(eulerRotation);
     private void Jump(Vector3 jumpForce) => _rigidbody.AddForce(jumpForce, ForceMode.Impulse);
     #endregion
 }
